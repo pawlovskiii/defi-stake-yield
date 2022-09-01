b7d9fb2eb8cb98c1f55876e877980bcc6356ab0c
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-09-01 22:07:25 +0200

    Create a module for getting a contract

---
+++ scripts/get_contract.py
@@ -0,0 +1,40 @@
+from brownie import (
+    network,
+    config,
+    MockV3Aggregator,
+    MockWETH,
+    MockDAI,
+    Contract,
+)
+
+NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["hardhat", "development", "ganache"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS + [
+    "mainnet-fork",
+    "binance-fork",
+    "matic-fork",
+]
+
+contract_to_mock = {
+    "eth_usd_price_feed": MockV3Aggregator,
+    "dai_usd_price_feed": MockV3Aggregator,
+    "fau_token": MockDAI,
+    "weth_token": MockWETH,
+}
+
+
+def get_contract(contract_name):
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in NON_FORKED_LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        contract = contract_type[-1]
+    else:
+        try:
+            contract_address = config["networks"][network.show_active()][contract_name]
+            contract = Contract.from_abi(contract_type._name, contract_address, contract_type.abi)
+        except KeyError:
+            print(
+                f"{network.show_active()} address not found, perhaps you should add it to the config or deploy mocks?"
+            )
+            print(f"brownie run scripts/deploy_mocks.py --network {network.show_active()}")
+    return contract
