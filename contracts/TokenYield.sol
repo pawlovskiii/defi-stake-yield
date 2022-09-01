52a83e5d5fd72da249570a9fb2119fbce1426924
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-09-01 22:26:52 +0200

    Change the name of the contract

--- contracts/TokenYield.sol
+++ contracts/TokenYield.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract TokenFarm is Ownable {
+contract TokenYield is Ownable {
     mapping(address => mapping(address => uint256)) public stakingBalance;
     mapping(address => uint256) public uniqueTokensStaked;
     mapping(address => address) public tokenPriceFeedMapping;
