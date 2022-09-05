99f367c56a9963aa318e0432e2d59bc42ca2690b
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-09-05 13:53:34 +0200

    Add a test for checking the correctness of the tokenIsAllowed function

--- tests/unit_testing/test_token_yield_contract.py
+++ tests/unit_testing/test_token_yield_contract.py
@@ -101,3 +101,14 @@ def test_add_allowed_tokens():
     assert yield_token.allowedTokens(0) == vistula_token.address
     with pytest.raises(exceptions.VirtualMachineError):
         yield_token.addAllowedTokens(vistula_token.address, {"from": non_owner})
+
+
+def test_token_is_allowed():
+    # Arrange
+    isNetworkLocal()
+    account = get_account()
+    yield_token, vistula_token = deploy_token_yield_and_vistula_token_contracts()
+    # Act
+    yield_token.tokenIsAllowed(vistula_token.address, {"from": account})
+    # Assert
+    assert yield_token.tokenIsAllowed(vistula_token.address) == True
