5962d4abc5f4dbd1ca7784fbebb804df69ba3e4d
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-09-05 18:19:21 +0200

    Add type hint to the function

--- scripts/helpful_modules/network_check.py
+++ scripts/helpful_modules/network_check.py
@@ -3,6 +3,6 @@ from scripts.helpful_modules.get_account import LOCAL_BLOCKCHAIN_ENVIRONMENTS
 from brownie import network
 
 
-def isNetworkLocal():
+def isNetworkLocal() -> None:
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip("Only for local testing!")
