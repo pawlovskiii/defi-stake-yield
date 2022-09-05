92cf88ed0368749342249cc091024cf387939da0
Jakub Pawlowski <jakubpawwwlowski@gmail.com>
2022-09-05 13:24:36 +0200

    Create a test case for testing rewards from staking within the contract

--- tests/unit_testing/test_token_yield_contract.py
+++ tests/unit_testing/test_token_yield_contract.py
@@ -3,6 +3,7 @@ from brownie import exceptions
 from scripts.helpful_modules.network_check import isNetworkLocal
 from scripts.helpful_modules.get_account import get_account
 from scripts.helpful_modules.get_contract import get_contract
+from scripts.helpful_modules.deploy_mocks import INITIAL_PRICE_FEED_VALUE
 from scripts.helpful_modules.deploy_token_yield_and_vistula_token import deploy_token_yield_and_vistula_token_contracts
 
 
@@ -32,3 +33,15 @@ def test_stake_tokens(amount_staked):
     assert yield_token.uniqueTokensStaked(account.address) == 1
     assert yield_token.stakers(0) == account.address
     return yield_token, vistula_token
+
+
+def test_issue_tokens(amount_staked):
+    # Arrange
+    isNetworkLocal()
+    account = get_account()
+    yield_token, vistula_token = test_stake_tokens(amount_staked)
+    starting_balance = vistula_token.balanceOf(account.address)
+    # Act
+    yield_token.issueTokens({"from": account}) 
+    # Arrange
+    assert(vistula_token.balanceOf(account.address) == starting_balance + INITIAL_PRICE_FEED_VALUE)
